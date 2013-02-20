5c4befd9954cbdf9c4ac5b2f4c733c939d6b620f
fmj <fmj200x@gmail.com>
2013-02-20 19:09:38 +0100

    godt i gang

---
+++ SimpleCalculator/SimpleCalculator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    public class SimpleCalculator
+    {
+        private char[] seperators = new char[] {',', '\n'};
+
+        public int Add(string calculation)
+        {
+
+            if (seperators.Count(c => calculation.IndexOf(c) >= 0) == 0)
+            {
+                return SafeParseInt(calculation);
+            }
+            else
+            {
+                string[] split = calculation.Split(seperators);
+                if(split.Count(m => m.Trim().Length == 0) > 0)
+                    throw new Exception("Life sux");
+                else
+                    return split.Sum(m => SafeParseInt(m));
+            }
+        }
+        
+
+        private int SafeParseInt(string input)
+        {
+            int value;
+
+            if (int.TryParse(input, out value))
+            {
+                return value;
+            }
+            return 0;
+        }
+    }
+}
--- SimpleCalculatorTest/UnitTest1.cs
+++ SimpleCalculatorTest/UnitTest1.cs
@@ -1,14 +1,68 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleCalculator;
 
 namespace SimpleCalculatorTest
 {
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        private SimpleCalculator.SimpleCalculator _calc;
+        [TestInitialize]
+        public void Setup()
         {
+            _calc = new SimpleCalculator.SimpleCalculator();
+        }
+
+        [TestMethod]
+        public void TestAddEmptyString()
+        {
+            
+            Assert.AreEqual(_calc.Add(""), 0);
+        }
+
+        [TestMethod]
+        public void TestAddOOneNumber()
+        {
+            Assert.AreEqual(_calc.Add("1"),1);
+        }
+
+        [TestMethod]
+        public void TestAddTwoNumber()
+        {
+            Assert.AreEqual(_calc.Add("1,3"),4);
+        }
+
+        [TestMethod]
+        public void TestAddXNumber()
+        {
+            Assert.AreEqual(_calc.Add("1,3,4,5,test"), 13);
+        }
+
+        [TestMethod]
+        public void TestAddNumberSplitNewLine()
+        {
+            Assert.AreEqual(_calc.Add("1\n2,3"),6);
+        }
+
+        [TestMethod]
+        public void TestMultipleSeperators()
+        {
+            try
+            {
+                _calc.Add("1,\n");
+            }
+            catch (Exception)
+            {
+                return;
+            }
+            Assert.Fail("No exception");
+        }
+
+        [TestMethod]
+        public void VerifyJustNewLIneSeperator()
+        {
+            Assert.AreEqual(_calc.Add("2\n3\n3"),8);
         }
     }
 }
