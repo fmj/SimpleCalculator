0ac0d4b73514ba88df36e024633d1ee5978c422a
fmj <fmj200x@gmail.com>
2013-02-20 19:57:40 +0100

    Done

--- SimpleCalculator/SimpleCalculator.cs
+++ SimpleCalculator/SimpleCalculator.cs
@@ -8,25 +8,57 @@ namespace SimpleCalculator
 {
     public class SimpleCalculator
     {
-        private char[] seperators = new char[] {',', '\n'};
+        private List<string> seperators = new List<string>() { ",", "\n" };
+
+        private const string CDefSeperator = "//";
 
         public int Add(string calculation)
         {
-
+            //noramlize input
+            if (calculation.StartsWith("//"))
+            {
+                //"//test\n1test2test3"
+                string newSep = calculation.Substring(CDefSeperator.Length,
+                                                      calculation.IndexOf('\n') - CDefSeperator.Length);
+                if (newSep.Contains("]["))
+                {
+                    string[] newSeps =
+                        newSep.TrimStart(new char[] { '[' }).TrimEnd(new char[] { ']' }).Split(new string[] { "][" }, StringSplitOptions.RemoveEmptyEntries);
+                    seperators.AddRange(newSeps.ToList());
+                }
+                else
+                    seperators.Add(newSep);
+                calculation = calculation.Substring(calculation.IndexOf('\n') + 1);
+            }
+            //do calculation
             if (seperators.Count(c => calculation.IndexOf(c) >= 0) == 0)
             {
                 return SafeParseInt(calculation);
             }
             else
             {
-                string[] split = calculation.Split(seperators);
-                if(split.Count(m => m.Trim().Length == 0) > 0)
+
+                string[] split = calculation.Split(seperators.ToArray(), StringSplitOptions.None);
+                if (split.Count(m => m.Trim().Length == 0) > 0)
                     throw new Exception("Life sux");
+                else if (split.Count(m => SafeParseInt(m) < 0) > 0)
+                {
+                    throw new Exception("Negative numbers detected, danger danger. The numbers are: " +
+                                        ReturnElems(split.Where(m => SafeParseInt(m) < 0).ToList()));
+                }
                 else
-                    return split.Sum(m => SafeParseInt(m));
+                    return split.Where(m => SafeParseInt(m) < 1000).Sum(m => SafeParseInt(m));
             }
+
         }
-        
+
+        public string ReturnElems(List<string> array)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (string s in array) sb.Append(s);
+            return sb.ToString();
+        }
+
 
         private int SafeParseInt(string input)
         {
--- SimpleCalculatorTest/UnitTest1.cs
+++ SimpleCalculatorTest/UnitTest1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SimpleCalculator;
 
@@ -7,6 +8,46 @@ namespace SimpleCalculatorTest
     [TestClass]
     public class UnitTest1
     {
+        [TestMethod]
+        public void TestMultipleNewDelims()
+        {
+            Assert.AreEqual(_calc.Add("//[*][%]\n1*2%3"),6);
+        }
+
+        [TestMethod]
+        public void TestMultipleNewLongDelims()
+        {
+            Assert.AreEqual(_calc.Add("//[Per][Erik]\n1Erik2Per3"), 6);
+        }
+        [TestMethod]
+        public void TestAnyLongSep()
+        {
+            Assert.AreEqual(_calc.Add("//test\n2test4test6"),12);
+        }
+        [TestMethod]
+        public void AddBigNumbers()
+        {
+            Assert.AreEqual(_calc.Add("1000,1001,2,4"),6);
+        }
+        [TestMethod]
+        public void AddNegativeNumberFail()
+        {
+            try
+            {
+                _calc.Add("-2,-3");
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                if(!(ex.Message.Contains("-2") && ex.Message.Contains("-3")))
+                {
+                    Assert.Fail("Errormessage did not contain the numbers");
+                }
+                return;
+            }
+            Assert.Fail("Negative numbers not recognized");
+        }
+
         private SimpleCalculator.SimpleCalculator _calc;
         [TestInitialize]
         public void Setup()
@@ -63,6 +104,12 @@ namespace SimpleCalculatorTest
         public void VerifyJustNewLIneSeperator()
         {
             Assert.AreEqual(_calc.Add("2\n3\n3"),8);
+        }
+
+        [TestMethod]
+        public void SupportRandomSeperators()
+        {
+            Assert.AreEqual(_calc.Add("//;\n1;2;3;4"),10);
         }
     }
 }
